1227c6778c3dd7666a48adf41c33db6ca9171a07
Alexander <33380655+Alex0424@users.noreply.github.com>
2024-10-20 00:05:46 +0200

    Update my_vars.sh

--- my_vars.sh
+++ my_vars.sh
@@ -2,6 +2,6 @@
 
 export portainer_username=<your_portainer_username>
 export portainer_password=<your_portainer_password>
-export db_pass="password"
+export db_pass="<your_password>"
 
 # source my_vars.sh
