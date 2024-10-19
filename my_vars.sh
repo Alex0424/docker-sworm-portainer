6f66ff75ed8e25102eda43ae870c52d20794cfe9
Alexander <33380655+Alex0424@users.noreply.github.com>
2024-10-20 00:09:07 +0200

    Update my_vars.sh

--- my_vars.sh
+++ my_vars.sh
@@ -5,3 +5,8 @@ export portainer_password=<your_portainer_password>
 export db_pass="<your_password>"
 
 # source my_vars.sh
+#
+# or upload your .env file e.g.:
+# $ cat .env
+# var1=secret1
+# var2=secret2
